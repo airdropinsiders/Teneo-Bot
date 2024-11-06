447ba87862803e0259ad5eb8b3569f28666dcad5
PrastianHD <prastianhidayat08@gmail.com>
2024-11-06 20:12:05 +0700

    Fix core.js

--- app/src/core/core.js
+++ app/src/core/core.js
@@ -2,127 +2,120 @@ import { HttpsProxyAgent } from 'https-proxy-agent';
 import { SocksProxyAgent } from 'socks-proxy-agent';
 import { Helper } from '../utils/helper.js';
 import { API } from './api/api.js';
-import a2_0x5ef004 from '../utils/logger.js';
 import WebSocket from 'ws';
-
+import a2_0x5ef004 from '../utils/logger.js';
 export default class Core extends API {
   constructor(_0x5425a9, _0x3f954d) {
     super('https://ikknngrgxuxgjhplbpey.supabase.co', _0x3f954d);
     this.acc = _0x5425a9;
     this.socket = null;
-    this.wssReconnectAttempts = 0;
-    this.maxwssReconnectAttempts = 5;
-    this.pingInterval = 0;
-    this.point = { pointsToday: 0, pointsTotal: 0 };
-    this.pingCount = 0;
+    this.wssReconnectAttempts = 0x0;
+    this.maxwssReconnectAttempts = 0x5;
+    this.pingInterval = 0x0;
+    this.point = {
+      'pointsToday': 0x0,
+      'pointsTotal': 0x0
+    };
+    this.pingCount = 0x0;
   }
-
-  async login() {
-    await Helper.delay(1000, this.acc, "Trying to log in...", this);
-    const response = await this.fetch('/auth/v1/token?grant_type=password', 'POST', undefined, {
-      email: this.acc.email,
-      password: this.acc.password,
-      gotrue_meta_security: {}
+  async ['login']() {
+    await Helper.delay(0x3e8, this.acc, "Try to login...", this);
+    const _0x4d25af = await this.fetch('/auth/v1/token?grant_type=password', 'POST', undefined, {
+      'email': this.acc.email,
+      'password': this.acc.password,
+      'gotrue_meta_security': {}
+    });
+    if (_0x4d25af.status == 0xc8) {
+      this.token = _0x4d25af.access_token;
+      this.refreshToken = _0x4d25af.refresh_token;
+      this.user = _0x4d25af.user;
+    } else {
+      this.handleError(_0x4d25af);
+    }
+  }
+  async ["getUser"]() {
+    await Helper.delay(0x3e8, this.acc, "Getting User Information...", this);
+    const _0x361956 = await this.fetch('/auth/v1/user', 'GET', this.token);
+    if (_0x361956.status == 0xc8) {
+      this.user = _0x361956;
+    } else {
+      this.handleError(_0x361956);
+    }
+  }
+  async connectWebSocket() {
+    let _0x16f12c;
+    if (this.proxy) {
+        if (this.proxy.startsWith('http')) {
+            _0x16f12c = new HttpsProxyAgent(this.proxy);
+        }
+        if (this.proxy.startsWith('socks')) {
+            _0x16f12c = new SocksProxyAgent(this.proxy);
+        }
+    }
+    this.socketURL = 'wss://secure.ws.teneo.pro/websocket?userId=' + this.user.id + '&version=v0.2';
+    this.socket = new WebSocket(this.socketURL, {
+        agent: _0x16f12c
     });
 
-    if (response.status === 200) {
-      this.token = response.access_token;
-      this.refreshToken = response.refresh_token;
-      this.user = response.user;
-    } else {
-      this.handleError(response);
-    }
-  }
-
-  async getUser() {
-    await Helper.delay(1000, this.acc, "Getting User Information...", this);
-    const response = await this.fetch('/auth/v1/user', 'GET', this.token);
-
-    if (response.status === 200) {
-      this.user = response;
-    } else {
-      this.handleError(response);
-    }
-  }
-
-  async connectWebSocket() {
-    let proxyAgent;
-
-    if (this.proxy) {
-      if (this.proxy.startsWith('http')) {
-        proxyAgent = new HttpsProxyAgent(this.proxy);
-      } else if (this.proxy.startsWith('socks')) {
-        proxyAgent = new SocksProxyAgent(this.proxy);
-      }
-    }
-
-    this.socketURL = `wss://secure.ws.teneo.pro/websocket?userId=${this.user.id}&version=v0.2`;
-    this.socket = new WebSocket(this.socketURL, { agent: proxyAgent });
-
     this.socket.onopen = async () => {
-      await Helper.delay(500, this.acc, "WebSocket connection opened.", this);
-      this.wssReconnectAttempts = 0;
-      this.startPing();
+        console.log("WebSocket connection opened.");
+        this.wssReconnectAttempts = 0;
     };
 
-    this.socket.onmessage = async (event) => {
-      const data = JSON.parse(event.data);
-      a2_0x5ef004.info(event.data);
-
-      await Helper.delay(2000, this.acc, "Received message: " + data.message, this);
-
-      if (data.pointsToday) {
-        this.point = { pointsToday: data.pointsToday, pointsTotal: data.pointsTotal };
-        await Helper.delay(1000, this.acc, "User Points Updated", this);
-      }
+    this.socket.onmessage = async (message) => {
+        const data = JSON.parse(message.data);
+        console.log("Received message:", data.message);
+        this.startPing();
     };
 
     this.socket.onerror = async (error) => {
-      await Helper.delay(500, this.acc, "WebSocket error: " + error, this);
+        console.error("WebSocket error:", error);
     };
 
     this.socket.onclose = async (event) => {
-      await Helper.delay(500, this.acc, `WebSocket connection closed${event.wasClean ? ' cleanly' : ' unexpectedly'}.`, this);
-      await this.stopPing();
-      await this.reconnectWebSocket();
+        if (event.wasClean) {
+            console.log("WebSocket connection closed cleanly.");
+        } else {
+            console.log("WebSocket connection closed unexpectedly.");
+        }
+        this.stopPing();
+        this.reconnectWebSocket();
     };
-  }
-
-  async reconnectWebSocket() {
+}
+  async ['reconnectWebSocket']() {
     if (this.wssReconnectAttempts < this.maxwssReconnectAttempts) {
-      this.wssReconnectAttempts += 1;
-      const reconnectDelay = Math.min(5000, 1000 * this.wssReconnectAttempts);
-      await Helper.delay(reconnectDelay, this.acc, `Attempting to reconnect (#${this.wssReconnectAttempts})...`, this);
-      await this.connectWebSocket();
+      this.wssReconnectAttempts += 0x1;
+      const _0x2349b9 = Math.min(0x1388, 0x3e8 * this.wssReconnectAttempts);
+      Helper.delay(_0x2349b9, this.acc, "Attempting to reconnect (#" + this.wssReconnectAttempts + ')...', this).then(() => this.connectWebSocket());
     } else {
-      await Helper.delay(1000, this.acc, "Max reconnect attempts reached. Could not reconnect to WebSocket.", this);
+      Helper.delay(0x3e8, this.acc, "Max reconnect attempts reached. Could not reconnect to WebSocket.", this);
     }
   }
-
-  async sendPing() {
-    if (this.socket && this.socket.readyState === WebSocket.OPEN) {
-      this.pingCount += 1;
-      await Helper.delay(0, this.acc, `Sending PING ${this.pingCount}`, this);
-      this.socket.send(JSON.stringify({ type: 'PING' }));
-    }
+  async ['sendPing']() {
+    this.pingCount = this.pingCount + 0x1;
+    await Helper.delay(0x0, this.acc, "Sending PING " + this.pingCount, this);
+    this.socket.send(JSON.stringify({
+      'type': 'PING'
+    }));
+    await Helper.delay(0x2710, this.acc, "PING " + this.pingCount + " Sended", this);
   }
-
-  startPing() {
+  async ['startPing']() {
     if (!this.pingInterval) {
-      a2_0x5ef004.info("Starting PING interval; Ping will be sent every 10 seconds");
-      this.pingInterval = setInterval(() => this.sendPing(), 10000); // Set to 10 seconds
+      await Helper.delay(0x0, this.acc, "Starting PING interval, Ping will be send every 10 Seconds", this);
+      this.pingInterval = setInterval(async () => {
+        if (this.socket && this.socket.readyState === WebSocket.OPEN) {
+          await this.sendPing();
+        }
+      }, 0x2710);
     }
   }
-
-  stopPing() {
+  async ['stopPing']() {
     if (this.pingInterval) {
       clearInterval(this.pingInterval);
       this.pingInterval = null;
-      a2_0x5ef004.info("Ping interval stopped");
     }
   }
-
-  handleError(error) {
-    throw error;
+  async ['handleError'](_0x5c291f) {
+    throw _0x5c291f;
   }
 }
